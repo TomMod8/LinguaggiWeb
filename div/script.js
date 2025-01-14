c4694408f92506bda0a09f524bfdafbb22c0ce7e
Tom Mod <tommaso.modolo@gmail.com>
2025-01-14 11:39:13 +0100

    Progresso con polaroid, aggiunta texture.

--- div/script.js
+++ div/script.js
@@ -1,5 +1,17 @@
-var num = 14;
-var str = "Hello World!";
+function scatterPhotos()
+{
+    var myArray = document.getElementsByClassName("polaroid");
 
-var sum = num + str;
-alert(sum);
+    for (var i=0; i < myArray.length; i++)
+    {
+        var posX = Math.floor(Math.random() * 700);
+        myArray[i].style.left = posX + "px";
+        var posY = Math.floor(Math.random() * 200);
+        myArray[i].style.top = posY + "px";
+        var rotation = Math.floor(Math.random() * 720) -360;
+        myArray[i].style.transform = "rotate(" + rotation + "deg)";
+    } 
+}
+
+scatterPhotos();
+
