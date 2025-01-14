968d2050d8fea3d4e310921ab013d2c4f94355e0
Tom Mod <tommaso.modolo@gmail.com>
2025-01-14 12:43:02 +0100

    Fix + prova bottone align

--- div/script.js
+++ div/script.js
@@ -6,10 +6,25 @@ function scatterPhotos()
     {
         var posX = Math.floor(Math.random() * 700);
         myArray[i].style.left = posX + "px";
+
         var posY = Math.floor(Math.random() * 200);
         myArray[i].style.top = posY + "px";
+
         var rotation = Math.floor(Math.random() * 720) -360;
         myArray[i].style.transform = "rotate(" + rotation + "deg)";
+    } 
+}
+
+function alignPhotos()
+{
+    var myArray = document.getElementsByClassName("polaroid");
+
+    for (var i=0; i < myArray.length; i++)
+    {
+        myArray[i].style.transform = "initial";
+
+        myArray[i].style.right = "300px"; //rivedi
+        myArray[i].style.top = "150px";
     } 
 }
 
