05172d4aae9a9236d178a416c422a9a05755a09b
Tom Mod <tommaso.modolo@gmail.com>
2025-01-15 13:02:00 +0100

    Continuo lavoro su Forms

--- form/script.js
+++ form/script.js
@@ -1,13 +1,17 @@
-
-var firstName = document.getElementById("firstN");
-var lastName = document.getElementById("lastN");
-
-var webhookURL = "https://webhook.site/514f908a-5272-4a40-96aa-8a8be86cb021";
-
-
-async function fetchRequest()
-{
-    const response = await fetch('data.json');
-    const fullName = await response.json();
-
+async function makeGET(URL) {
+    const response = await fetch(URL, {mode:"no-cors"});
+    //aggiunta secondo parametro a fetch per evitare errori
 }
+
+function confirmData()
+{
+    var firstName = document.getElementById("firstNJS").value;
+    var lastName = document.getElementById("lastNJS").value;
+    const baseURL = "https://webhook.site/514f908a-5272-4a40-96aa-8a8be86cb021";
+
+    var fullURL = (baseURL + "?" + "fname=" + firstName + "&lname=" + lastName);
+    
+    makeGET(fullURL);
+}
+
+
