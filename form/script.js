b078e350eebe6bd09c3e89e625671effa0280489
Tom Mod <tommaso.modolo@gmail.com>
2025-01-16 11:07:32 +0100

    Prosegui lavoro forms

--- form/script.js
+++ form/script.js
@@ -1,6 +1,6 @@
 async function makeGET(URL) {
     const response = await fetch(URL, {mode:"no-cors"});
-    //aggiunta secondo parametro a fetch per evitare errori
+    //aggiunta secondo parametro a fetch per evitare errori CORS
 }
 
 function confirmData()
@@ -12,6 +12,43 @@ function confirmData()
     var fullURL = (baseURL + "?" + "fname=" + firstName + "&lname=" + lastName);
     
     makeGET(fullURL);
+
+    document.getElementById("firstNJS").value = "";
+    document.getElementById("lastNJS").value = "";
+    //svuoto campi dopo click
+}
+
+function saveLocal()
+{
+    var firstName = document.getElementById("firstNBottom").value;
+    var lastName = document.getElementById("lastNBottom").value;
+
+    localStorage.setItem("firstName", firstName);
+    localStorage.setItem("lastName", lastName);
+    //setto la variabile inserita nei campi nella memoria del browser
+
+    document.getElementById("firstNBottom").value = "";
+    document.getElementById("lastNBottom").value = "";
+    //svuoto campi dopo averli salvati sul browser
+}
+
+function welcomeBack()
+{   
+    var firstName = localStorage.getItem("firstName");
+    var lastName = localStorage.getItem("lastName");
+    // || check se è nullo, uguale a if ... != null
+
+    if(firstName=="" && lastName=="")
+    {
+        alert("Welcome!");
+    }
+    else
+    {
+        alert("Welcome! Last local user name is: " + firstName + "!");
+        var answer = prompt("Is that you? Y/N");
+        if(answer=="y" || answer=="Y") alert("Welcome back " + firstName + "!");
+            else alert("Logging off...");
+    }
 }
 
 
