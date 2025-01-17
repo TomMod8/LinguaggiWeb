416649177db3681239b4a7ec513445bb4e7416cb
Tom Mod <tommaso.modolo@gmail.com>
2025-01-17 01:12:31 +0100

    Chat implementata + Indicatore status connessione.

--- MQTT/PahoStart.js
+++ MQTT/PahoStart.js
@@ -1,17 +1,27 @@
+var brokerURL;
+var clientID;
+var topicName;
+var brokerPort;
+
+var client;
+
+document.getElementById("area").value = ""; //pulisci textarea al refresh
 
 function getInputConnect()
 {
-  var brokerURL = document.getElementById("broker").value;
-  var clientID = document.getElementById("client").value;
-  var topicName = document.getElementById("topic").value;
+  brokerURL = document.getElementById("broker").value;
+  brokerPort = document.getElementById("port").value;
+  clientID = document.getElementById("client").value;
+  topicName = document.getElementById("topic").value;
 
   alert("Data was registered!");
   document.getElementById("broker").value = "";
   document.getElementById("client").value = "";
   document.getElementById("topic").value = "";
+  document.getElementById("port").value = "";
 
   // Create a client instance
-  var client = new Paho.Client(brokerURL, Number(8080), clientID);
+  client = new Paho.Client(brokerURL, Number(brokerPort), clientID);
 
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
@@ -19,10 +29,9 @@ function getInputConnect()
 
   // connect the client
   client.connect({onSuccess:onConnect});
-
-  client.subscribe(topicName);
-
 }
+
+
 
 function sendMessage()
 {
@@ -31,7 +40,7 @@ function sendMessage()
   alert("Message sent!");
   document.getElementById("messageBox").value = "";
 
-  message = new Paho.Message(messagestring);   //rivedi accesso a  topic name + client. va rifatto
+  message = new Paho.Message(messagestring);
   message.destinationName = topicName;
   client.send(message);
 }
@@ -41,17 +50,22 @@ function sendMessage()
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
   console.log("onConnect");
+  client.subscribe(topicName);
+  document.getElementById("status").style.backgroundColor = "green";
+  document.getElementById("connectButton").value = "Connected!";
 }
 
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
     console.log("onConnectionLost:"+responseObject.errorMessage);
+    document.getElementById("status").style.backgroundColor = "red";
+    document.getElementById("connectButton").value = "Connect";
   }
 }
 
 // called when a message arrives
 function onMessageArrived(message) {
     const messageArea = document.getElementById("area");
-    messageArea.value += message.payloadString + "\n";
+    messageArea.value += " Someone says: " + message.payloadString + "\n";
 }
